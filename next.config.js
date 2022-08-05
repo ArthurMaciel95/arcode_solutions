1998cf828740974df666ebf7bcea88e52953f21a
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-05 10:24:44 -0300

    :lipstick: adicionando efeito de scala na imagem de portifolio

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  i18n: {
+    /**
+     * Provide the locales you want to support in your application
+     */
+    locales: ["en-US", "pt-BR", "es-ES"],
+    /**
+     * This is the default locale you want to be used when visiting
+     * a non-locale prefixed path.
+     */
+    defaultLocale: "pt-BR",
+  },
+
 }
 
 module.exports = nextConfig
