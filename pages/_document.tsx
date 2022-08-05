4448915aaee21f4bb12273b4a0444f0621dc860b
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-05 03:45:53 -0300

    :chart_with_upwards_trend: adicionando robots.txt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="scroll-smooth">
+      <Html lang="pt">
         <Head>
           <meta
             name="description"
