bb8072adf89a40c764503363ac1ad28167596c96
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-13 18:30:22 -0300

    :dizzy: adicionando animações nas seções

--- src/components/offer/index.tsx
+++ src/components/offer/index.tsx
@@ -1,33 +1,36 @@
 import * as S from "./styles";
 import Button from "@mui/material/Button";
 import Image from "next/image";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 const Offer = () => {
   return (
-    <S.Container>
-      <h2>
-        Crie seu site na
-        <br /> <span>ARCODE SOLUÇÕES</span>
-      </h2>
-      <h3>
-        SITE TOTALMENTE PERSONALIZADO
-        <br /> APARTIR DE R$599,90
-      </h3>
+    <AnimationOnScroll animateIn="animate__fadeIn" animatePreScroll={false}>
+      <S.Container>
+        <h2>
+          Crie seu site na
+          <br /> <span>ARCODE SOLUÇÕES</span>
+        </h2>
+        <h3>
+          SITE TOTALMENTE PERSONALIZADO
+          <br /> APARTIR DE R$599,90
+        </h3>
 
-      <Button
-        variant="whatsapp"
-        sx={{ padding: "20px 30px", fontSize: "2rem" }}
-        href="https://wa.me/5521989578285?text=Eu%20gostaria%20de%20fazer%20um%20orçamento."
-      >
-        <Image
-          src="/tmp/svg/whatsapp.svg"
-          width="50px"
-          height="50px"
-          alt="icone whatsapp"
-          priority
-        />
-        Faça um orçamento conosco agora!
-      </Button>
-    </S.Container>
+        <Button
+          variant="whatsapp"
+          sx={{ padding: "20px 30px", fontSize: "2rem" }}
+          href="https://wa.me/5521989578285?text=Eu%20gostaria%20de%20fazer%20um%20orçamento."
+        >
+          <Image
+            src="/tmp/svg/whatsapp.svg"
+            width="50px"
+            height="50px"
+            alt="icone whatsapp"
+            priority
+          />
+          Faça um orçamento conosco agora!
+        </Button>
+      </S.Container>
+    </AnimationOnScroll>
   );
 };
 
--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -1,44 +1,46 @@
 import React from "react";
 import * as S from "./styles";
 import CardServices from "../service-card";
-
+import { AnimationOnScroll } from "react-animation-on-scroll";
 const Services: React.FC = () => {
   return (
-    <S.Container id="services">
-      <h2>BENEFÍCIOS</h2>
-      <div className="card-area">
-        <CardServices
-          imageUrl="/tmp/image/localizacao.png"
-          description="Aumente o tamanho do seu mercado!"
-          alt="ilustração de uma tela de site com ícone de localização"
-        />
-        <CardServices
-          imageUrl="/tmp/image/metricas_tables.png"
-          description="Reduza seus custos de divulgação."
-          alt="ilustração de uma tela de site com ícone de metricas"
-        />
-        <CardServices
-          imageUrl="/tmp/image/computador_check.png"
-          description="Aumente o seu 'léque' de canais para divulgar seu produto.(Site, instagram, facebook)."
-          alt="ilustração de uma tela de seite com um computador"
-        />
-        <CardServices
-          imageUrl="/tmp/image/metricas.png"
-          description="Tenha controle e informações de seus. clientes através de metricas do google."
-          alt="ilustração de uma tela de site com metricas;"
-        />
-        <CardServices
-          imageUrl="/tmp/image/engrenagem.png"
-          description="Oferecemos site com ótimo SEO para um melhor ranqueamento nas pesquisas do google."
-          alt="ilustração de engrenagem"
-        />
-        <CardServices
-          imageUrl="/tmp/image/responsividade.png"
-          description="Seu site funcionando em diversos dispositivos."
-          alt="ilustração mostrando diversos dispositivos mostrando um site."
-        />
-      </div>
-    </S.Container>
+    <AnimationOnScroll animateIn="animate__fadeIn" animatePreScroll={false}>
+      <S.Container id="services">
+        <h2>BENEFÍCIOS</h2>
+        <div className="card-area">
+          <CardServices
+            imageUrl="/tmp/image/localizacao.png"
+            description="Aumente o tamanho do seu mercado!"
+            alt="ilustração de uma tela de site com ícone de localização"
+          />
+          <CardServices
+            imageUrl="/tmp/image/metricas_tables.png"
+            description="Reduza seus custos de divulgação."
+            alt="ilustração de uma tela de site com ícone de metricas"
+          />
+          <CardServices
+            imageUrl="/tmp/image/computador_check.png"
+            description="Aumente o seu 'léque' de canais para divulgar seu produto.(Site, instagram, facebook)."
+            alt="ilustração de uma tela de seite com um computador"
+          />
+          <CardServices
+            imageUrl="/tmp/image/metricas.png"
+            description="Tenha controle e informações de seus. clientes através de metricas do google."
+            alt="ilustração de uma tela de site com metricas;"
+          />
+          <CardServices
+            imageUrl="/tmp/image/engrenagem.png"
+            description="Oferecemos site com ótimo SEO para um melhor ranqueamento nas pesquisas do google."
+            alt="ilustração de engrenagem"
+          />
+          <CardServices
+            imageUrl="/tmp/image/responsividade.png"
+            description="Seu site funcionando em diversos dispositivos."
+            alt="ilustração mostrando diversos dispositivos mostrando um site."
+          />
+        </div>
+      </S.Container>
+    </AnimationOnScroll>
   );
 };
 
