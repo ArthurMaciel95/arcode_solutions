789e7cd1ee22fd82cf4ffea3d0ed82bf586b4ecf
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-14 03:54:25 -0300

    :zap: alterando local de arquivo para pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require("next-pwa");
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    sw: "sw.js",
     dest: "public",
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
   },
 });
 
