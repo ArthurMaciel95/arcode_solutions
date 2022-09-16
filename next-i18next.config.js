a34b4f636481fed48f627253dfa0871ea965adfd
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-09-16 15:07:18 -0300

    update: nextjs.config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,9 +4,6 @@ const path = require("path");
 module.exports = {
   i18n: {
     locales: ["en", "pt", "es"],
-    defaultLocale: "pt",
-    localeDetection: false,
-    localePath: path.resolve("./public/locales")
-  },
-  reloadOnPrerender: process.env.NODE_ENV === "development"
+    defaultLocale: "pt"
+  }
 };
--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { i18n } = require("./next-i18next.config");
+const i18n = require("./next-i18next.config");
 
 /* @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"]
   },
-  i18n
+  ...i18n
 };
 
 module.exports = nextConfig;
